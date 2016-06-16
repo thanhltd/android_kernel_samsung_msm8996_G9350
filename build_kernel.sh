b66d44b4aac0093b23f7c9e51c18213ab0fb65d5
msdx321 <msdx321@gmail.com>
2016-06-16 13:55:30 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,13 @@ export CROSS_COMPILE=$(pwd)/toolchains/linux-x86/aarch64/aarch64-linux-android-4
 export ARCH=arm64
 export SEC_BUILD_OPTION_HW_REVISION=02
 
+rm -rf out
 mkdir out
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android msdx321_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android -j8
+
+dtbToolCM -v -s 2048 -o out/arch/arm64/boot/dt.img  out/arch/arm64/boot/dts/samsung/
+
+mv out/arch/arm64/boot/dt.img out/dt.img
+mv out/arch/arm64/boot/Image.gz out/kernel.gz
