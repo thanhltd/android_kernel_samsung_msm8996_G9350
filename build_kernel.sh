47e53fb91a289e18b8a8b649e1c800ef40683161
msdx321 <msdx321@gmail.com>
2016-06-16 10:33:19 +0800

    Misc Change

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-export PATH=/home/bit/Android/CyanogenMod/cm-13.0/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
-export CROSS_COMPILE=/home/bit/Android/CyanogenMod/cm-13.0/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export PATH=$(pwd)/toolchains/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
+export CROSS_COMPILE=$(pwd)/toolchains/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
 export SEC_BUILD_OPTION_HW_REVISION=02
 
-rm -rf ~/out
-mkdir ~/out
+mkdir out
 
-make -C $(pwd) O=/home/bit/out KCFLAGS=-mno-android msdx321_defconfig
-make -C $(pwd) O=/home/bit/out KCFLAGS=-mno-android -j8
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android msdx321_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android -j8
